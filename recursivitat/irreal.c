3cc3cd1735e065ee2f87340f58b1138f62fbc063
nico09125 <nicrenchu@alu.edu.gva.es>
2024-12-05 14:57:29 +0100

    Update irreal.c

--- recursivitat/irreal.c
+++ recursivitat/irreal.c
@@ -4,7 +4,7 @@ int irreal(int n);
 
 void main(){
     int n;
-    printf("Introduix el número: ");
+    printf("Introduce el número: ");
     scanf("%d", &n);
     if(irreal(n)) printf("\nIrreal\n");
     else printf("\nNo irreal\n");
@@ -12,11 +12,11 @@ void main(){
 }
 
 int irreal(int n){
-    int aux, unitats, decenes;
-    unitats = n%10;
-    decenes = (n/10)%10;
+    int aux, unidades, decenas;
+    unidades = n%10;
+    decenas = (n/10)%10;
     if(n/10 == 0) aux=1;
-       else if(unitats >= decenes) aux = irreal(n/10);
+       else if(unidades >= decenas) aux = irreal(n/10);
             else aux = 0;
     return aux;
 }
